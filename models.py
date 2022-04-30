a784fdb39b3f70c192e6da9946bf71ab5424b73e
Lucas Carr <crrluc003@myuct.ac.za>
2022-05-01 01:50:53 +0200

    added CNN

--- models.py
+++ models.py
@@ -1,34 +1,39 @@
+# CSC3022 Assignment 4
+# Lucas Carr
+# CRRLUC003
+# models.py
 
-from torch import nn
 import torch.nn.functional as F
+from torch import nn
+
 """
 Model 1:
     - 784, 32, 10
-    - Learning Rate: 1e-1 (0.1)
-    - Epochs: 20
-    - Batch Size = 100
     - Activation Function: Sigmoid 
     - Loss Function: CrossEntropyLoss
     - Optimizer: SGD
+    - Learning Rate: 0.01
+    - Epochs: 10
+    - Batch Size = 150
+
 """
 class Model_1(nn.Module):
     model_name = "Model 1"
     model_layers = [784, 32, 10]
-    learning_rate = 1e-1
-    epochs = 20
-    batch_size = 100
-
+    learning_rate = 1e-2
+    epochs = 10
+    batch_size = 150
+    activation_function = "Sigmoid"
     loss_function = nn.CrossEntropyLoss()
-
+    
     def __init__(self):
         super(Model_1, self).__init__()
-
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(Model_1.model_layers[0], Model_1.model_layers[1]),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.Linear(Model_1.model_layers[1], Model_1.model_layers[1]),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.Linear(Model_1.model_layers[1], Model_1.model_layers[2]),
         )
 
@@ -39,21 +44,21 @@ class Model_1(nn.Module):
 
 """
 Model 2:
-    - 784, 512, 10
-    - Learning Rate: 1e-1 (0.1)
-    - Epochs: 20
-    - Batch Size = 64
-    - Activation Function: ReLU 
+    - 784, 32, 10
+    - Learning Rate: 1e-1 (0.1)     (change)
+    - Epochs: 16
+    - Batch Size = 150 
+    - Activation Function: ReLU     (change)
     - Loss Function: CrossEntropyLoss
     - Optimizer: SGD
 """
 class Model_2(nn.Module):
     model_name = "Model 2"
-    model_layers = [784, 512, 10]
+    model_layers = [784, 32, 10]
     learning_rate = 1e-1
-    epochs = 20
-    batch_size = 64
-
+    epochs = 16
+    batch_size = 150
+    activation_function = "ReLU"
     loss_function = nn.CrossEntropyLoss()
 
     def __init__(self):
@@ -75,22 +80,22 @@ class Model_2(nn.Module):
 
 """
 Model 3:
-    - 784, 512, 10
-    - Learning Rate: 1e-1 (0.1)
-    - Epochs: 20
-    - Batch Size = 32
+    - [784, 256, 10]                (change)
+    - Learning Rate: 1e-1 (0.01)    (change)
+    - Epochs: 15
+    - Batch Size = 32               (change)
     - Activation Function: ReLU 
-    - Loss Function: CrossEntropyLoss
-    - Optimizer: SGD
+    - Loss Function: NLLL           (change)
+    - Optimizer: SGD               
 """
 class Model_3(nn.Module):
     model_name = "Model 3"
-    model_layers = [784, 512, 10]
+    model_layers = [784, 256, 10]
     learning_rate = 1e-1
-    epochs = 20
+    epochs = 15
     batch_size = 32
-
-    loss_function = nn.CrossEntropyLoss()
+    activation_function = "ReLU"
+    loss_function = nn.NLLLoss()
 
     def __init__(self):
         super(Model_3, self).__init__()
@@ -102,6 +107,7 @@ class Model_3(nn.Module):
             nn.Linear(Model_3.model_layers[1], Model_3.model_layers[1]),
             nn.ReLU(),
             nn.Linear(Model_3.model_layers[1], Model_3.model_layers[2]),
+            nn.LogSoftmax(dim=1),
         )
 
     def forward(self, x):
@@ -111,22 +117,23 @@ class Model_3(nn.Module):
 
 """
 Model 4:
-    - 784, 512, 10
-    - Learning Rate: 1e-1 (0.1)
-    - Epochs: 5
+    - 784, 512, 256, 10]                    (change)
+    - Learning Rate: 0.005                  (change)
+    - Epochs: 5                             (change)
     - Batch Size = 32
-    - Activation Function: ReLU 
-    - Loss Function: CrossEntropyLoss
-    - Optimizer: SGD
+    - Activation Function: ReLU, 
+        ReLU, ReLU, LogSoftMax              (change)
+    - Loss Function: NLLLoss                (change)
+    - Optimizer: Adam                       (change)
 """
 class Model_4(nn.Module):
     model_name = "Model 4"
-    model_layers = [784, 512, 10]
-    learning_rate = 1e-1
-    epochs = 5
+    model_layers = [784, 512, 256, 10]
+    learning_rate = 0.01
+    epochs = 10
     batch_size = 32
-
-    loss_function = nn.CrossEntropyLoss()
+    activation_function = "ReLU"
+    loss_function = nn.NLLLoss()
 
     def __init__(self):
         super(Model_4, self).__init__()
@@ -134,10 +141,19 @@ class Model_4(nn.Module):
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(Model_4.model_layers[0], Model_4.model_layers[1]),
+            nn.BatchNorm1d(Model_4.model_layers[1]),
             nn.ReLU(),
-            nn.Linear(Model_4.model_layers[1], Model_4.model_layers[1]),
-            nn.ReLU(),
+
             nn.Linear(Model_4.model_layers[1], Model_4.model_layers[2]),
+            nn.BatchNorm1d(Model_4.model_layers[2]),
+            nn.ReLU(),
+
+            nn.Linear(Model_4.model_layers[2], Model_4.model_layers[2]),
+            nn.BatchNorm1d(Model_4.model_layers[2]),
+            nn.ReLU(),
+
+            nn.Linear(Model_4.model_layers[2], Model_4.model_layers[3]),
+            nn.LogSoftmax(dim=1),
         )
 
     def forward(self, x):
@@ -160,42 +176,14 @@ Model 5:
 class Model_5(nn.Module):
     model_name = "Model 5"
     model_layers = [784, 100, 10]
-    learning_rate = 1e-1
-    epochs = 5
+    learning_rate = 1e-3
+    epochs = 10
     batch_size = 32
-
-    loss_function = nn.CrossEntropyLoss()
+    activation_function = "ReLU"
+    loss_function = nn.NLLLoss()
 
     def __init__(self):
         super(Model_5, self).__init__()
-
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(Model_5.model_layers[0], Model_5.model_layers[1]),
-            nn.ReLU(),
-            nn.BatchNorm1d(Model_5.model_layers[1]),
-            nn.Linear(Model_5.model_layers[1], Model_5.model_layers[1]),
-            nn.ReLU(),
-            nn.BatchNorm1d(Model_5.model_layers[1]),
-            nn.Linear(Model_5.model_layers[1], Model_5.model_layers[2]),
-            nn.ReLU(),
-        )
-
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-
-class Model_6(nn.Module):
-    model_name = "Model 6"
-    model_layers = [784, 100, 10]
-    learning_rate = 1e-3
-    epochs = 5
-    batch_size = 32
-    loss_function = nn.CrossEntropyLoss()
-    
-    def __init__(self):
-        super(Model_6, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
         self.conv3 = nn.Conv2d(32,64, kernel_size=5)
@@ -204,6 +192,7 @@ class Model_6(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        #x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(F.max_pool2d(self.conv3(x),2))
@@ -213,3 +202,4 @@ class Model_6(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+

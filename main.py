38e53880b6e606c69c3183a4254f9d26e6ed1637
Lucas Carr <crrluc003@myuct.ac.za>
2022-05-01 01:51:17 +0200

    Added functionality to train/test model multiple times and average results

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# CSC3022 Assignment 4
+# Lucas Carr
+# CRRLUC003
+# main.py
+
 import numpy as np 
 import torch
-from torch import nn
 import torch.utils.data as data
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
@@ -10,33 +14,58 @@ import sys
 import models
 from model_trainer import model_trainer as mt
 
-training_data = datasets.MNIST('data', train=True, download=False, transform=ToTensor())            #training data (size = 60 000), split up into training, validation
-test_data = datasets.MNIST('data', train=False, download=False, transform=ToTensor())               #test data (unseen by the model)
+
+training_data = datasets.MNIST('data', train=True, download=True, transform=ToTensor())            #training data (size = 60 000), split up into training, validation
+test_data = datasets.MNIST('data', train=False, download=True, transform=ToTensor())               #test data (unseen by the model)
 training_data, validation_data = data.random_split(training_data, (48000, 12000))  
+
 
 if __name__=='__main__':
     model_number = sys.argv[1]
     model_number = int(model_number) - 1
 
-    model_lists = [
+    summation = 0
+    #testing
+    for i in range (1):
+        model_lists = [
         models.Model_1(),
         models.Model_2(),
         models.Model_3(),
         models.Model_4(),
         models.Model_5(),
-        models.Model_6(),
         ]
 
-    model = model_lists[model_number]
-    optimizer = torch.optim.SGD(model.parameters(), lr = model.learning_rate)
+        model = model_lists[model_number]
+        if model_number < 3:        #-1 for index offput
+            optimizer = torch.optim.SGD(model.parameters(), lr = model.learning_rate)
+            optimizer_string = "SGD"
+        else: 
+            optimizer = torch.optim.Adam(model.parameters(),lr=model.learning_rate,betas=(0.9,0.999),
+                eps=1e-08,weight_decay=0,amsgrad=False)
+            optimizer_string = "Adam"
+            
+        #Printing Model Details
+        print (model.model_name,":", sep="")
+        print ("Topology: ", model.model_layers)
+        print ("Activation Function: ", model.activation_function)
+        print ("Loss Function: ", model.loss_function)
+        print ("Optimizer: ", optimizer_string)
+        print ("Batch Size: ", model.batch_size)
+        print ("Learning Rate: ", model.learning_rate)
+        print ("---"*11)
 
-    print (model.model_name)
-    for e in range (model.epochs):
-        print (f"Epoch number {e+1}\n")
-        mt.train_model(model, training_data, optimizer)
-        mt.validate_model(model, validation_data)
+        #Training
+        for e in range (model.epochs):
+            print (f"Epoch number: {e+1}\n")
+            mt.train_model(model, training_data, optimizer)
+            mt.validate_model(model, validation_data)
+            print ("---"*11)
 
-    mt.test_model(model, test_data)
+        print("Finished Training")
+        x = mt.test_model(model, test_data)
+        summation += x
+    
+    print (summation, summation/20)
 
     response = ""
     while True:
